7a128b37d4b9de3e5b98acb168318b3615cc2e5c
koushiksavudam <savudamkoushik@gmail.com>
2020-08-26 17:32:15 +0530

    second commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+@Component({
+    selector:'app-server',
+    templateUrl:'./server.component.html',
+    styles:[`
+    .online{
+        color:white;
+    }
+    `]
+})
+export class ServerComponent {
+    serverId:number = 10;
+    serverStatus:String="offline";
+    constructor(){
+        this.serverStatus=Math.random()>0.5?'Online':'Offline';
+    }
+    getColor(){
+        return this.serverStatus==='Online'?'blue':'red';
+    }
+} 
